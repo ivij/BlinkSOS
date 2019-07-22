e493ce5de515d03140f3bae8386521d712ff6a68
ivij <53103290+ivij@users.noreply.github.com>
2019-07-22 22:06:48 +1000

    Add files via upload

---
+++ morse_code_SOS.ino
@@ -0,0 +1,42 @@
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  //for s //
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);                       
+//for O //
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  //for s //
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);digitalWrite(LED_BUILTIN, HIGH);   
+  delay(500);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);
+}
